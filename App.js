6192d5617b6b04d0405aa006122f96e13bceb87a
Yoel-ui-yeon <yanniealanunay116@gmail.com>
2026-04-25 13:56:01 +0800

    feat: implement redux reducer and saga for auth state management

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from './src/app/reducers/index';
+import rootSaga from './src/app/saga/index';
 import RootNavigation from './src/navigations/index';
 
+const { store, runSaga } = configureStore();
+runSaga(rootSaga);
+
 const App = () => {
-  return <RootNavigation />;
+  return (
+    <Provider store={store}>
+      <RootNavigation />
+    </Provider>
+  );
 };
 
 export default App;
